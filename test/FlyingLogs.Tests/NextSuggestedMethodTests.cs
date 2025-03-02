809421b392061ffbee36ca30010ac3195ae3f54d
Bekir Ozturk <60651445+bekir-ozturk@users.noreply.github.com>
2025-03-02 19:30:38 +0000

    Any method name can increase the suggested method name number

--- test/FlyingLogs.Tests/NextSuggestedMethodTests.cs
+++ test/FlyingLogs.Tests/NextSuggestedMethodTests.cs
@@ -80,4 +80,23 @@ Log.Information.L3("hello", time: System.DateTime.UtcNow);
             """public const string Boo4_ = "This field exists to hint you a unique method name. Use it to trigger code completion and remove the underscore afterwards.";"""
         )));
     }
+
+    [Test]
+    public void AnyMethodNameCounts()
+    {
+        Environment.SetEnvironmentVariable(
+            "FLYINGLOGS_SUGGESTED_METHOD_PREFIX",
+            "Foo",
+            EnvironmentVariableTarget.Process);
+
+        Helpers.Compile(out var compilation, out var diagnostics, """
+using FlyingLogs;
+Log.Information.L3("hello", time: System.DateTime.UtcNow);
+Log.Information.Hey7("hello");
+""");
+
+        Assert.That(compilation.SyntaxTrees.Any(t => t.GetText().ToString().Contains(
+            """public const string Foo8_ = "This field exists to hint you a unique method name. Use it to trigger code completion and remove the underscore afterwards.";"""
+        )));
+    }
 }
